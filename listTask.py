4a1c1910aa9b897e6f15d81ae62de2657f62960f
JosephEmeka <josephemekaonah@gmail.com>
2024-02-12 21:36:53 +0100

    new additions

--- listTask.py
+++ listTask.py
@@ -1,22 +1,41 @@
-def thislist():
+def this_list():
+    new_list = []
     for number in range(1, 16):
-        print(list(number))
+        new_list.append(number)
+    return new_list
 
 
 def duplicate_list(sample_list):
     new_list = []
+    my_duplicate_list = []
     for number in range(1, 16):
         new_list.append(number)
-        new_list.appemd(number)
-    return new_list
+        my_duplicate_list = new_list + new_list
+    return my_duplicate_list
 
 
 def eliminate_duplicates(sample_list):
-    return set(sample_list)
+    new_list = []
+    set_of_numbers = set(sample_list)
+    for number in set_of_numbers:
+        new_list.append(number)
+    return new_list
 
 
-def add_every_third_element_(sample_list):
+def add_every_third_element(sample_list):
     total = 0
     for number in sample_list[2::3]:
         total += number
+    return total
+
+
+def sum_first_middle_and_last_element(my_list):
+    total = 0
+    middle_sum = 0
+    middle_index = len(my_list) // 2
+    if len(my_list) % 2 == 0:
+       for number in my_list[(middle_index-1): (middle_index+1)]:
+           middle_sum += number
+       middle_number = middle_sum / 2
+    total = my_list[0] + my_list[-1] + middle_number
     return total
--- test/test_listTask.py
+++ test/test_listTask.py
@@ -1,10 +1,32 @@
-from pythonProject1 import *
+import unittest
+from unittest import TestCase
+import listTask
+from listTask import *
+
+
 
 
 class TestlistTask(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_function_create_list_from_numbers_ranging_from_One_to_fifteen_function(self):
+        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        self.assertEqual(my_list, this_list())
 
+    def test_function_that_duplicate_list_item(self):
+        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+        expected_result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                           14, 15]
+        self.assertEqual(expected_result, duplicate_list(my_list))
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_function_that_eliminates_duplicates_in_list_item(self):
+        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 12, 13, 14, 15, 16, 17]
+        expected_result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+        self.assertEqual(expected_result, eliminate_duplicates(my_list))
+
+    def test_function_that_add_every_third_element(self):
+        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(18, add_every_third_element(my_list))
+
+    def test_function_that_calculate_sum_of_first_middle_and_last_number_in_a_list(self):
+        my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(16.5, sum_first_middle_and_last_element(my_list))
+
